d0325a2e2fed2c94ed35fe0e49fc6106aeee4870
Angel <43154124+angel-utn@users.noreply.github.com>
2025-03-17 21:01:34 -0300

    Archivos Clase 01

---
+++ Clase 01/Clase01.sql
@@ -0,0 +1,35 @@
+ Create Database Clase01
+ Use Clase01
+
+Create Table Areas(
+	IDArea smallint not null primary key identity (1, 1),
+	Nombre varchar(50) not null,
+	Mail varchar(255) not null
+)
+
+Create Table Proyectos(
+	IDProyecto int not null primary key,
+	Nombre varchar(100) not null
+)
+
+Create TAble Empleados(
+	IDEmpleado int not null primary key,
+	Apellidos varchar(100) not null,
+	Nombres varchar(100) not null,
+	Salario money check (Salario >= 0),
+	FechaContratacion date not null,
+	IDArea smallint null foreign key references Areas(IDArea)
+)
+
+Create Table EmpleadosProyecto(
+	IDEmpleado int not null foreign key references Empleados(IDEmpleado),
+	IDProyecto int not null foreign key references Proyectos(IDProyecto),
+	primary key (IDEmpleado, IDProyecto)
+)
+
+Create TAble PresupuestosAreas(
+	Año tinyint not null,
+	IDArea smallint not null foreign key references Areas(IDArea),
+	Presupuesto money not null check (Presupuesto >= 0),
+	primary key (Año, IDArea)
+)
